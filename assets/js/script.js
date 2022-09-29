7c4c68629d7cbc8afb52239b18d2c1e9895faab9
Kayla Justice <kaylarjustice@gmail.com>
2022-09-29 14:15:37 -0500

    Brewery class created.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,42 @@
 
 // Global variable
 let coords = 0;
+let breweryTest;
+class Brewery{
+    constructor(blob){
+        console.log(blob);
+        this.name = blob.name;
+        this.phone = blob.phone;
+        this.url = blob.website_url;
+        this.city = blob.city;
+        this.address = (blob.street + " " + blob.city + " " + blob.state + " " + blob.postal_code);
+    }
+    getName() {
+        return this.name;
+    }
+}
+
+
+/*
+0: 
+address_2: null
+address_3: null
+brewery_type: "micro"
+city: "Saint Cloud"
+country: "United States"
+county_province: null
+created_at: "2022-08-20T02:56:08.975Z"
+id: "beaver-island-brewing-company-saint-cloud"
+latitude: "45.5585455"
+longitude: "-94.15733312"
+name: "Beaver Island Brewing Company"
+phone: "3202535907"
+postal_code: "56301-4304"
+state: "Minnesota"
+street: "216 6th Ave S"
+updated_at: "2022-08-20T02:56:08.975Z"
+website_url: "http://www.beaverislandbrew.com"
+*/
 
 
 // On page load ask for location, if not prompt input
@@ -58,9 +94,15 @@ function getBreweryByZipCode(zipCode){//handle multiple pages
         return response.json();
     }).then(data=>{
         console.log(data);
+        test(data);        
     }).catch(function(error){
-        console.log("Errorr in the api request 2", error);
+        console.log("Error in the api request 2", error);
     });
+}
+
+function test(data) {
+    let brewery = new Brewery(data[0]);
+    console.log(brewery);
 }
 
 // Get weather by zipcode
@@ -93,11 +135,6 @@ function getWeatherByZipCode(zipCode){
         console.log("", error);
     })
 
-   
-
-
-
-
     // const encodedParams = new URLSearchParams();
     // encodedParams.append("apiKey", "<REQUIRED>");
     // encodedParams.append("query", "zipCode");
@@ -117,4 +154,34 @@ function getWeatherByZipCode(zipCode){
     //     .then(response => console.log(response))
     //     .catch(err => console.error(err));
 
+}
+
+let tempLocation = JSON.parse(localStorage.getItem("location-id"));
+let tempForecast = JSON.parse(localStorage.getItem("forecast"));
+
+function tempWeatherStorage() {
+    
+    if (!localStorage.getItem("forecast")){
+        
+        fetch(`http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=%09cDt63DqtaKBplCaTQdLTUsKRTCQZaAYi&q=45.5%2C-94.1`).then(response =>{
+            return response.json();
+        }).then(data =>{
+            console.log(data);
+            localStorage.setItem("location-id", JSON.stringify(data));
+            key = data.Key;
+            console.log(key);
+            
+        fetch(`http://dataservice.accuweather.com/forecasts/v1/daily/1day/${key}?apikey=%09cDt63DqtaKBplCaTQdLTUsKRTCQZaAYi`).then(response =>{
+                return response.json();
+            }).then(data =>{
+                console.log(data);
+                localStorage.setItem("forecast", JSON.stringify(data));
+            }).catch(function(error){
+                console.log("", error);
+            })          
+            
+            }).catch(function(error){
+                console.log("", error);
+        })
+    }      
 }
