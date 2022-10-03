2a99d7e625f1de23d0a9b80105c7c2a69c6fb7c3
Luis T <luistarantini@gmail.com>
2022-10-03 15:02:27 -0500

    added more content to the index.html and fixed js

--- assets/js/godhelpme.js
+++ assets/js/godhelpme.js
@@ -1,80 +1,94 @@
-function boxToggle() {
-    var x = document.getElementById("toggle");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
+$('#facts').on('click',function(event){
+  if($(event.target).attr("class")== "fold"){
+      if($(event.target).next('div').css("display")  == "block"){
+          $(event.target).next('div').css("display","none");
+      }else{
+          $(event.target).next('div').css("display","block");
+      }
   }
+});
 
-  function boxToggle2() {
-    var x = document.getElementById("toggle2");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
 
-  function boxToggle3() {
-    var x = document.getElementById("toggle3");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
 
-  function boxToggle4() {
-    var x = document.getElementById("toggle4");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
 
-  function boxToggle5() {
-    var x = document.getElementById("toggle5");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
+// function boxToggle(id) {
+//     var x = document.getElementById(id);
+//     var y = x.getAttribute("target");
+//     if (x.style.display === "none") {
+//       x.style.display = "block";
+//     } else {
+//       x.style.display = "none";
+//     }
+//   }
 
-  function boxToggle6() {
-    var x = document.getElementById("toggle6");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
+//   function boxToggle2() {
+//     var x = document.getElementById("toggle2");
+//     if (x.style.display === "none") {
+//       x.style.display = "block";
+//     } else {
+//       x.style.display = "none";
+//     }
+//   }
 
-  function boxToggle7() {
-    var x = document.getElementById("toggle7");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
+//   function boxToggle3() {
+//     var x = document.getElementById("toggle3");
+//     if (x.style.display === "none") {
+//       x.style.display = "block";
+//     } else {
+//       x.style.display = "none";
+//     }
+//   }
 
-  function boxToggle8() {
-    var x = document.getElementById("toggle8");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
+//   function boxToggle4() {
+//     var x = document.getElementById("toggle4");
+//     if (x.style.display === "none") {
+//       x.style.display = "block";
+//     } else {
+//       x.style.display = "none";
+//     }
+//   }
 
-  function boxToggle9() {
-    var x = document.getElementById("toggle9");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
+//   function boxToggle5() {
+//     var x = document.getElementById("toggle5");
+//     if (x.style.display === "none") {
+//       x.style.display = "block";
+//     } else {
+//       x.style.display = "none";
+//     }
+//   }
+
+//   function boxToggle6() {
+//     var x = document.getElementById("toggle6");
+//     if (x.style.display === "none") {
+//       x.style.display = "block";
+//     } else {
+//       x.style.display = "none";
+//     }
+//   }
+
+//   function boxToggle7() {
+//     var x = document.getElementById("toggle7");
+//     if (x.style.display === "none") {
+//       x.style.display = "block";
+//     } else {
+//       x.style.display = "none";
+//     }
+//   }
+
+//   function boxToggle8() {
+//     var x = document.getElementById("toggle8");
+//     if (x.style.display === "none") {
+//       x.style.display = "block";
+//     } else {
+//       x.style.display = "none";
+//     }
+//   }
+
+//   function boxToggle9() {
+//     var x = document.getElementById("toggle9");
+//     if (x.style.display === "none") {
+//       x.style.display = "block";
+//     } else {
+//       x.style.display = "none";
+//     }
+//   }
