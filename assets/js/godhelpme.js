ca916bb92bf7bc8e39a1c7f6b4472f0c1654ec2f
Luis T <luistarantini@gmail.com>
2022-10-03 12:49:09 -0500

    added more content to index and fixed a couple things

--- assets/js/godhelpme.js
+++ assets/js/godhelpme.js
@@ -24,3 +24,57 @@ function boxToggle() {
       x.style.display = "none";
     }
   }
+
+  function boxToggle4() {
+    var x = document.getElementById("toggle4");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function boxToggle5() {
+    var x = document.getElementById("toggle5");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function boxToggle6() {
+    var x = document.getElementById("toggle6");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function boxToggle7() {
+    var x = document.getElementById("toggle7");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function boxToggle8() {
+    var x = document.getElementById("toggle8");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function boxToggle9() {
+    var x = document.getElementById("toggle9");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
