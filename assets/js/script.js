c0808643597abfae87cac8fc052ec0f3b6accd4d
Hunter <realisvenator@gmail.com>
2022-10-04 00:11:46 -0500

    feature complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,8 +7,10 @@ const brewerySearchResults = document.querySelector('#brewery-search-results');
 
 // Global variables
 const states = {"alaska": "ak","alabama": "al","arkansas": "ar","american samoa": "as","arizona": "az","california": "ca","colorado": "co","connecticut": "ct","district of columbia": "dc","delaware": "de","florida": "fl","georgia": "ga","guam": "gu","hawaii": "hi","iowa": "ia","idaho": "id","illinois": "il","indiana": "in","kansas": "ks","kentucky": "ky","louisiana": "la","massachusetts": "ma","maryland": "md","maine": "me","michigan": "mi","minnesota": "mn","missouri": "mo","mississippi": "ms","montana": "mt","north carolina": "nc","north dakota": "nd","nebraska": "ne","new hampshire": "nh","new jersey": "nj","new mexico": "nm","nevada": "nv","new york": "ny","ohio": "oh","oklahoma": "ok","oregon": "or","pennsylvania": "pa","puerto rico": "pr","rhode island": "ri","south carolina": "sc","south dakota": "sd","tennessee": "tn","texas": "tx","utah": "ut","virginia": "va","virgin islands": "vi","vermont": "vt","washington": "wa","wisconsin": "wi","west virginia": "wv","wyoming": "wy"};
-let coords = 0;
-let breweryTest;
+var coords = 0;
+var brewerys=[];
+var games=[];
+
 class Brewery{
     constructor(blob){
         this.name = blob.name;
@@ -17,31 +19,48 @@ class Brewery{
         this.city = blob.city;
         this.address = (blob.street + " " + blob.city + " " + blob.state + " " + blob.postal_code);
     }
-    getName() {
-        return this.name;
+    toHtml(){
+        let tempCard = $('<div>');
+        tempCard.attr('class','card');
+        let tempBody = $('<div>');
+        tempBody.attr('class','card-body');
+        tempBody.append($('<h3>').text("Name: "+this.name));
+        tempBody.append($('<p>').text("City: "+this.city));
+        tempBody.append($('<p>').text("PhoneNumber: "+this.phone));
+        tempBody.append($('<p>').text("Addres: "+this.address));
+        tempBody.append($('<p>').text("Website: "+this.url));
+        tempCard.on("click",function(){
+            console.log("memes")
+            window.location.href="./page3.html?games=yes";
+        });
+        return tempCard.append(tempBody);
     }
 }
 
-/*
-0: 
-address_2: null
-address_3: null
-brewery_type: "micro"
-city: "Saint Cloud"
-country: "United States"
-county_province: null
-created_at: "2022-08-20T02:56:08.975Z"
-id: "beaver-island-brewing-company-saint-cloud"
-latitude: "45.5585455"
-longitude: "-94.15733312"
-name: "Beaver Island Brewing Company"
-phone: "3202535907"
-postal_code: "56301-4304"
-state: "Minnesota"
-street: "216 6th Ave S"
-updated_at: "2022-08-20T02:56:08.975Z"
-website_url: "http://www.beaverislandbrew.com"
-*/
+class Game{
+    constructor(blob){ //Constructor of game class. 
+        this.name=blob.name;
+        this.howtTo=blob.howtTo;
+        this.img=blob.img;
+    }
+    toHtml(){ //Takes a game obejct and returns html to display it.
+        let tempCard = $('<div>').attr("class","cardflip");
+        let tempInner = $('<div>').attr("class","innercardflip");
+        let tempFront = $('<div>').attr("class","cardflip-front");
+        tempFront.append($('<h1>').text(this.name));
+        tempFront.append($('<img>').attr("src",`http://mineboss.asuscomm.com:56733/images/?name=${this.img}`).attr("alt",`Picture of ${this.img.split("."[0])}`).attr("width","200px").attr("height","200px"));
+        let tempBack = $('<div>').attr("class","cardflip-back");
+        tempBack.append($('<h3>').text("Watch this to learn how to play!"));
+        tempBack.append($('<iframe>').attr("id","ytplayer").attr("type","text/html").attr("width","200px").attr("height","125px").attr("src",`${this.howtTo}`));
+        tempInner.append(tempFront);
+        tempInner.append(tempBack)
+        tempCard.append(tempInner);
+        tempCard.on('click',function(){//listener on the card for the flip.
+            $(this).toggleClass('hover');
+        });
+        return tempCard;
+    }
+}
 
 // If the browser doesn't support geolocation block the button from being shown.
 let tempLocation = JSON.parse(localStorage.getItem("location-id"));
@@ -76,53 +95,21 @@ async function getZipFromLatLong (){
     }
 }
 
-async function getBreweriesByZipCode(zipCode,numberOfBreweries=1){
+async function getBreweriesByZipCode(zipCode,numberOfBreweries=5){
     try{
         const response = await fetch(`https://api.openbrewerydb.org/breweries?by_postal=${zipCode}&per_page=${numberOfBreweries}`);
         const data = await response.json();
-        console.log(data);
-        // return new Brewery(data);
+        for(let b of data){
+            let tempB = new Brewery(b);
+            brewerys.push(tempB);
+        }
+        populateBrews()
+        
     }catch(e){
         console.log("Error in fetching breweries ", e);
     }    
-    // console.log(data);
-    displayBreweries(data);     // Is this the correct location to call this function?
+    // console.log(data);     // Is this the correct location to call this function?
 }
-
-function test(data) {
-    let brewery = new Brewery(data[0]);
-    console.log(brewery);
-}
-
-async function testLocal(){
-    try{
-        const response = await fetch(`http://mineboss.asuscomm.com:56733/games`);
-        const data = await response.json();
-        console.log(data);
-        // return new Brewery(data);
-    }catch(e){
-        console.log("Error in fetching breweries ", e);
-    }    
-}
-
-
-function validState(){
-    let state=$('#state').val();
-    if(states[state]){
-        console.log(states[state]);
-        validCity(states[state]);
-
-    }else{
-        alert("Please enter a valid state.");
-    }
-}
-
-// function populateZips(blob){
-//     console.log(blob["place name"]);
-//     $('#cityDisplay').text(blob["place name"]);
-
-// }
-
 async function validCity(state){
     let city=$('#city').val();
     
@@ -132,18 +119,27 @@ async function validCity(state){
             alert("Please enter a valid city.");
         }else{
             const data = await response.json();
-            data.places[0]["post code"];
-            console.log(data.places[0]["post code"]);
-            getBreweriesByZipCode(data.places[0]["post code"]);
+            console.log("2,"+data.places[0]["post code"]);
             console.log(tempForecast);
-            //getWeatherByZipCode would be called here but we are using the temp storage for now
-            // populateZips(data);
-            // console.log(data);
+            window.location.href = `./page2.html?zipcode=${data.places[0]["post code"]}`;
+
         }
     }catch(e){
         console.log("Failed to get postal code. ",e);
     }
 }
+
+function validState(){
+    let state=$('#state').val();
+    if(states[state]){
+        console.log("1,"+states[state]);
+        validCity(states[state]);
+
+    }else{
+        alert("Please enter a valid state.");
+    }
+}
+
 
 // Get weather by zipcode
 async function getWeatherByZipCode(zipCode){
@@ -197,50 +193,44 @@ async function tempWeatherStorage(){
     }
 }
 
-// Need to connect to html page 2
-function displayBreweries(data){
-    console.log(data);
-    
-    // set up `<div>` to hold result content (this is the parent `<div>`)
-    let resultCard = document.createElement('div');
-    resultCard.classList.add('card', /* add CSS styling classes */);
-
-    // create body `<div>` and append to resultCard
-    let resultBody = document.createElement('div');
-    resultBody.classList.add('card-body', /* add CSS styling classes */);
-    resultCard.append(resultBody);
-
-    // create brewery name `<h3>`
-    let breweryNameEl = document.createElement('h3');
-    // breweryNameEl.classList.add('h3', /* add CSS styling classes */);
-    breweryNameEl.textContent = "Name: " + data.name;
-
-    // create city `<p>`
-    let breweryCityEl = document.createElement('p');
-    // breweryCityEl.classList.add('p', /* add CSS styling classes */);
-    breweryCityEl.textContent = "City: " + data.city;
-
-    // create phone number `<p>`
-    let breweryPhoneEl = document.createElement('p');
-    // breweryPhoneEl.classList.add('p', /* add CSS styling classes */);
-    breweryPhoneEl.textContent = "Phone number: " + data.phone;
-
-    // create address `<p>`
-    let breweryAddressEl = document.createElement('p');
-    // breweryAddressEl.classList.add('p', /* add CSS styling classes */);
-    breweryAddressEl.textContent = "Address: " + data.street + " " + data.city + " " + data.state + " " + data.postal;
-
-    // create url `<p>`
-    let breweryWebsiteEl = document.createElement('p');
-    // breweryWebsiteEl.classList.add('p', /* add CSS styling classes */);
-    breweryWebsiteEl.textContent = "Website: " + data.website_url;
-
-    // append brewery information to resultBody
-    resultBody.append(breweryNameEl, breweryCityEl, breweryPhoneEl, breweryAddressEl, breweryWebsiteEl);
-
-    // apend entire contents of resultCard to brewerySearchResults `<div>` in page2.html
-    brewerySearchResults.append(resultCard);
+async function populateGames(){//grabs game list from api objectifys them.
+    let tempTarget = $('.games-lists');
+    try{
+        const response = await fetch(`http://mineboss.asuscomm.com:56733/games`);
+        const data = await response.json();
+        console.log(data);
+        for(let i of data){
+            let x =new Game(i);
+            console.log(x.toHtml());
+            tempTarget.append(x.toHtml());
+        }
+        // return new Brewery(data);
+    }catch(e){
+        console.log("Error in fetching breweries ", e);
+    }    
 }
+
+function populateBrews(){
+    let tempTarget = $('#brewery-search-results');
+    console.log(tempTarget);
+    console.log(brewerys);
+    console.log(brewerys.length);
+    for(let b in brewerys){
+        console.log("ZZZZZZZZZZZ"+brewerys[b]);
+        tempTarget.append(brewerys[b].toHtml());
+    }
+}
+
+$('#facts').on('click',function(event){
+    if($(event.target).attr("class")== "fold"){
+        if($(event.target).next('div').css("display")  == "block"){
+            $(event.target).next('div').css("display","none");
+        }else{
+            $(event.target).next('div').css("display","block");
+        }
+    }
+  });
+  
 
 //Listeners
 
@@ -249,9 +239,23 @@ $('#getGeo').on('click',function(){
     navigator.geolocation.getCurrentPosition(showLocation, promptLocationInput);
 });
 
-// Search button listener
-// When the user clicks the search button, what needs to happen?
-    // User is redirected to second page
-    // getBreweriesByZip function is initiated
-    // displayBreweries is called after page redirect
-$('#search-button').on('click', getBreweriesByZipCode);
+$(function(){
+    console.log(window.location.search);
+    let urlP = new URLSearchParams(window.location.search);
+    if(urlP.get("zipcode")){
+        let urlP = new URLSearchParams(window.location.search);
+        getBreweriesByZipCode(urlP.get("zipcode"));
+    }
+    if(urlP.get("games")){
+        populateGames();
+    }
+
+});
+
+$(document).ready(function() {  
+
+    $('.cardflip').click(function() {
+        $(this).toggleClass('hover');
+    });
+  
+  });
